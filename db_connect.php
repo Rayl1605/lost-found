6ea130158b65f5e1fc47e1032a5a8a1bea17fa68
Raylly Almendras <rayllylalmendras@gmail.com>
2026-02-07 14:45:15 +0800

    Connect to cloud database and prepare for Vercel

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,11 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "lost_found_db";
+$host = 'mysql-2020e1cf-rayllylalmendras-0230.j.aivencloud.com';
+$user = 'avnadmin';         
+$pass = 'AVNS_9keDpfhiZkvFpPmSCzA'; 
+$db   = 'defaultdb';       
+$port = '19987';            
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
